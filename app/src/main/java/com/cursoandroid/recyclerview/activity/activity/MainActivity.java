9d109a451b0513fd542728a297379f01d0a05e68
Kaic Carvalho <kaic15_fernandes@hotmail.com>
2021-01-07 11:09:53 -0300

    Criada a lista de Filmes.

--- app/src/main/java/com/cursoandroid/recyclerview/activity/activity/MainActivity.java
+++ app/src/main/java/com/cursoandroid/recyclerview/activity/activity/MainActivity.java
@@ -7,10 +7,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import com.cursoandroid.recyclerview.R;
+import com.cursoandroid.recyclerview.activity.adapter.Adapter;
+import com.cursoandroid.recyclerview.activity.model.Filme;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private List<Filme> listaFilme = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +26,53 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
 
+        //Listagem de filmes
+        this.criarFilmes();
+
         //configurar o Adapter
-
-
-
+        Adapter adapter = new Adapter();
 
         //Configurar o recyclerView
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext()); // LinearLayout ou GridLayout
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        // LinearLayout ou GridLayout
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true); //Recomendação Google
-        //recyclerView.setAdapter(adaptador);
+        recyclerView.setAdapter(adapter); // Adapter instanciado
 
     }
+
+    public void criarFilmes(){
+
+        Filme filme = new Filme("Homem-Aranha: Longe de Casa", "Ação/Aventura", "2019");
+        this.listaFilme.add(filme);
+
+        filme = new Filme("Capitã Marvel", "Ação/Ficção científica", "2019");
+        this.listaFilme.add(filme);
+
+        filme = new Filme("Vingadores: Guerra Infinita", "Ação/Ficção científica", "2018");
+        this.listaFilme.add(filme);
+
+        filme = new Filme("Pantera Negra", "Ação/Aventura", "2018");
+        this.listaFilme.add(filme);
+
+        filme = new Filme("Thor: Ragnarok", "Ação/Aventura", "2017");
+        this.listaFilme.add(filme);
+
+        filme = new Filme("Guardiões da Galáxia Vol. 2", "Ação/Ficção científica", "2017");
+        this.listaFilme.add(filme);
+
+        filme = new Filme("Vingadores: Ultimato", "Ação/Ficção científica", "2019");
+        this.listaFilme.add(filme);
+
+        filme = new Filme("Homem-Formiga e a Vespa", "Ação/Aventura", "2018");
+        this.listaFilme.add(filme);
+
+        filme = new Filme("Doutor Estranho", "Ação/Fantasia", "2016");
+        this.listaFilme.add(filme);
+
+        filme = new Filme("Capitão América: Guerra Civil", "Ação/Aventura", "2016");
+        this.listaFilme.add(filme);
+
+    }
+
 }
--- app/src/main/java/com/cursoandroid/recyclerview/activity/adapter/Adapter.java
+++ app/src/main/java/com/cursoandroid/recyclerview/activity/adapter/Adapter.java
@@ -1,5 +1,6 @@
 package com.cursoandroid.recyclerview.activity.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,22 +8,34 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.cursoandroid.recyclerview.R;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
     @NonNull
     @Override
+    //Cria a visualização
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View itemLista = LayoutInflater.from(parent.getContext())
+                //Inflar = Converter XML para visualização
+                .inflate(R.layout.adapter_lista, parent, false);
+
+        return new MyViewHolder(itemLista);
     }
 
     @Override
+    //Exibe os ítens,
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.titulo.setText("Título teste");
+        holder.genero.setText("Comédia");
+        holder.ano.setText("2017");
     }
 
     @Override
+    //Quantidade de ítens que serão exibidos
     public int getItemCount() {
-        return 0;
+        return 5;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
@@ -31,7 +44,16 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            //itemView acessa o objeto View
+
+            titulo = itemView.findViewById(R.id.textTitulo);
+            ano = itemView.findViewById(R.id.textAno);
+            genero = itemView.findViewById(R.id.textGenero);
+
         }
     }
+
+
 
 }
